08d99a0e789a713b9ba81379fe7a8224acfb7911
esquilofreniko <esquilofreniko@gmail.com>
2019-04-19 14:52:56 +0100

    test

---
+++ site.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+  background(0);
+  textSize(width / 64);
+  stroke(255);
+  fill(255);
+  text("theneuronproject",0,height/32);
+}
+
+
+function draw() {
+}
